e3890fe3e3f3367935ef9a0d70425bad3dda7221
zeynelerenkinali <zeynel.eren.kinali55@erzurum.edu.tr>
2025-01-27 08:52:23 +0300

    end of the project

--- yolo/car_counter.py
+++ yolo/car_counter.py
@@ -10,6 +10,7 @@ if not cap.isOpened():
 
 
 model = YOLO("yolo_weights/yolov8l.pt")
+totalCount = []
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -32,13 +33,15 @@ mask = cv2.imread("yolo/mask.png")
 # Tracking
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
-limits = (223, 375, 673, 375)
+limits = (400, 297, 673, 297)
 
 while True:
     success, img = cap.read()
     imgRegion = cv2.bitwise_and(img, mask)
     results = model(imgRegion, stream=True)
 
+    imgGraphics = cv2.imread("yolo/graphics.png", cv2.IMREAD_UNCHANGED)
+    cvzone.overlayPNG(img,imgGraphics, (0,0))
     detections = np.empty((0, 5))
 
     for r in results:
@@ -63,8 +66,6 @@ while True:
             currentClass = classNames[int(cls)]
             
             if currentClass == "car" or currentClass == "truck" or currentClass == "bus" or currentClass == "motorbike" and conf > 0.3:
-                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(13,x1+13),max(30,y1-15)), scale=0.8, thickness=1, offset=3)
-                # cvzone.cornerRect(img,bbox, l=5, rt=3)
                 currentArray = np.array([x1, y1, x2, y2, conf])
                 detections = np.vstack((detections, currentArray))
 
@@ -73,17 +74,21 @@ while True:
 
     for result in resultsTracker:
         x1, y1, x2, y2, id = result
-        x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2) 
-        #cvzone.putTextRect(img, f'{currentClass} {Id}', (max(13,x1+13),max(30,y1-15)), scale=0.8, thickness=1, offset=3)
+        x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+
+        w, h = x2-x1,y2-y1
+        bbox = int(x1),int(y1),int(w),int(h)
         cvzone.cornerRect(img, bbox, l=5, rt=3, colorR=(255,0,0))
         cvzone.putTextRect(img, f' {int(id)}', (max(13,x1+13),max(30,y1-15)), scale=2, thickness=3, offset=10)
         print(result)
         
         cx, cy = x1+w//2, y1+h//2
         cv2.circle(img,(cx,cy), 5, (255,0,255), cv2.FILLED)
-
-
+        if limits[0] < cx < limits[2] and limits[1] - 15 < cy < limits[1] + 15:
+            if(totalCount.count(id) == 0):
+                totalCount.append(id)
+                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
+        cv2.putText(img, str(len(totalCount)),(255,100), cv2.FONT_HERSHEY_PLAIN, 4, (255,0,0), 8)
     cv2.imshow("Image", img)
-    #cv2.imshow("ImageRegion", imgRegion)
-    cv2.waitKey(0) # when we turn waitkey to zero we can move by keyboard
+    cv2.waitKey(1) # when we turn waitkey to zero we can move by keyboard
 
